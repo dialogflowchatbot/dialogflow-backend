e8e6a692e1acde071e6859792bb2da349ebba8c0
dialogflowchatbot <dialogflow@yeah.net>
2024-02-29 17:09:13 +0800

    Updated frontend files

--- src/web/asset.rs
+++ src/web/asset.rs
@@ -4,8 +4,8 @@ use once_cell::sync::Lazy;
 
 pub(crate) static ASSETS_MAP: Lazy<HashMap<&str, usize>> = Lazy::new(|| {
 HashMap::from([
-("/assets/index-3syoVsug.js", 0),
-("/assets/index-iCGkRJ-1.css", 1),
+("/assets/index-93NIe4JJ.css", 0),
+("/assets/index-9D092EYe.js", 1),
 ("/favicon.ico", 2),
 ("/", 3),
 ("/index.html", 3),
