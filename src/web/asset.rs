f761755141983e6accfcb390e0b7f9316c5150d5
dialogflowchatbot <dialogflow@yeah.net>
2024-06-10 16:09:43 +0800

    Update front-end files

--- src/web/asset.rs
+++ src/web/asset.rs
@@ -4,10 +4,13 @@ use once_cell::sync::Lazy;
 
 pub(crate) static ASSETS_MAP: Lazy<HashMap<&str, usize>> = Lazy::new(|| {
     HashMap::from([
-        (r"/assets/index-DXJ5f3Oq.js", 0),
-        (r"/assets/index-X6IkoCLM.css", 1),
-        (r"/favicon.ico", 2),
-        ("/", 3),
-        (r"/index.html", 3),
+        (r"/assets/inbound-bot-PJJg_rST.png", 0),
+        (r"/assets/index-B2kCm4YY.css", 1),
+        (r"/assets/index-CI0yDkE7.js", 2),
+        (r"/assets/outbound-bot-EmsLuWRN.png", 3),
+        (r"/assets/text-bot-CWb_Poym.png", 4),
+        (r"/favicon.ico", 5),
+        ("/", 6),
+        (r"/index.html", 6),
     ])
 });
