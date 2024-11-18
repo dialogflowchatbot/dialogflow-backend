d4838e6e9508419552f862647a47c3d877c6438f
dialogflowchatbot <dialogflow@yeah.net>
2024-11-18 23:25:43 +0800

    Migrated to another database

--- src/db/embedding.rs
+++ src/db/embedding.rs
@@ -1,16 +1,7 @@
-use arrow_array::types::Float32Type;
-use arrow_array::{FixedSizeListArray, Int32Array, RecordBatch, RecordBatchIterator};
-use arrow_schema::{DataType, Field, Schema};
-use futures::TryStreamExt;
-
-use lancedb::arrow::IntoArrow;
-use lancedb::connection::Connection;
-use lancedb::index::Index;
-use lancedb::query::{ExecutableQuery, QueryBase};
-use lancedb::{connect, Error, Result, Table as LanceDbTable};
-
-async fn add() -> Result<()> {
-    let db = connect("data/embeddings").execute().await?;
-    let table = db.open_table("my_table").execute().await?;
-    Ok(())
+fn init_datasource() {
+    unsafe {
+        libsqlite3_sys::sqlite3_auto_extension(Some(std::mem::transmute(
+            sqlite_vec::sqlite3_vec_init as *const (),
+        )));
+    }
 }
